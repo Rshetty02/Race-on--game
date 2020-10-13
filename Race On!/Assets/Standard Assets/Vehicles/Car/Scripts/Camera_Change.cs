af0163527419cb7331cf4942ce22c9121c7fb871
Rshetty02 <70402171+Rshetty02@users.noreply.github.com>
2020-10-13 23:13:08 +0530

    Added camera control

---
+++ Race On!/Assets/Standard Assets/Vehicles/Car/Scripts/Camera_Change.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Change : MonoBehaviour
+{
+    public GameObject Third_person_cam;
+    public GameObject First_person_cam;
+    public int Cam_mode;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Cam_mode==1){
+            Cam_mode = 0;
+        }
+        else{
+            Cam_mode += 1;
+        }
+
+    }
+
+    IEnumerator ModeChange(){
+        yield return new WaitForSeconds(0.01f);
+        if(Cam_mode==0){
+            Third_person_cam.SetActive(true);
+            First_person_cam.SetActive(false);
+        }
+        if(Cam_mode == 1){
+            Third_person_cam.SetActive(false);
+            First_person_cam.SetActive(true);
+            
+
+        }
+
+
+    }
+}
