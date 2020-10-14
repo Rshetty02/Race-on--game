e47470da0645c1740790609793720026ae38afb6
Rshetty02 <70402171+Rshetty02@users.noreply.github.com>
2020-10-14 22:08:42 +0530

    added camera change

---
+++ Race On!/Assets/Scripts/Camera_Change.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Change : MonoBehaviour
+{
+    public GameObject Third_person_cam;
+    public GameObject First_person_cam;
+    public int Cam_mode;
+
+
+    public void OnButtonPress(){
+      
+        if(Cam_mode==1){
+            Cam_mode = 0;
+        }
+        else{
+            Cam_mode += 1;
+        }
+    StartCoroutine(ModeChange());
+   }
+
+    // Update is called once per frame
+   
+
+    IEnumerator ModeChange(){
+        yield return new WaitForSeconds(0.01f);
+        if(Cam_mode==0){
+            Third_person_cam.SetActive(true);
+            First_person_cam.SetActive(false);
+        }
+        if(Cam_mode == 1){
+            Third_person_cam.SetActive(false);
+            First_person_cam.SetActive(true);
+            
+
+        }
+
+
+    }
+}
