0aed3a7e0fd6390d6ea99adf17b6eaaef642ef61
Rshetty02 <70402171+Rshetty02@users.noreply.github.com>
2020-10-22 20:10:10 +0530

    Added mountains and trees

--- Race On!/Assets/start_game_scene.cs
+++ Race On!/Assets/start_game_scene.cs
@@ -5,9 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class start_game_scene : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start game
     public void startgame(){
         SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    // track select
+    public void trackselect(){
+        SceneManager.LoadScene("trackselect");
+    } 
+
+    public void mainmenu(){
+        SceneManager.LoadScene("Main Menu");
+
+    }
+
+    public void mountainbackroad(){
+        SceneManager.LoadScene("Level02");
 
     }
 
