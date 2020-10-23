0948105065edb2232f2ba097d4831a417f54bfc9
Rshetty02 <70402171+Rshetty02@users.noreply.github.com>
2020-10-24 00:21:41 +0530

    Increased Track Length, Fixed AI Car Bug

--- Race On!/Assets/Scripts/WaypointTrackerAI.cs
+++ Race On!/Assets/Scripts/WaypointTrackerAI.cs
@@ -22,10 +22,31 @@ public class WaypointTrackerAI : MonoBehaviour
     public GameObject mark12;
     public GameObject mark13;
 
+    public GameObject mark14;
+
+    public GameObject mark15;
+
+    public GameObject mark16;
+    public GameObject mark17;
+    public GameObject mark18;
+    public GameObject mark19;
+    public GameObject mark20;
+    public GameObject mark21;
+    public GameObject mark22;
+    public GameObject mark23;
+    public GameObject mark24;
+    public GameObject mark25;
+    public GameObject mark26;
+    public GameObject mark27;
+    public GameObject mark28;
+    public GameObject mark29;
+    public GameObject mark30;
+    public GameObject mark31;
+
     public int MarkTracker;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(MarkTracker==0){
             TheMarker.transform.position = mark01.transform.position;
@@ -72,6 +93,60 @@ public class WaypointTrackerAI : MonoBehaviour
         if(MarkTracker==12){
             TheMarker.transform.position = mark13.transform.position;
         }
+        if(MarkTracker==13){
+            TheMarker.transform.position = mark14.transform.position;
+        }
+        if(MarkTracker==14){
+            TheMarker.transform.position = mark15.transform.position;
+        }
+        if(MarkTracker==15){
+            TheMarker.transform.position = mark16.transform.position;
+        }
+        if(MarkTracker==16){
+            TheMarker.transform.position = mark17.transform.position;
+        }
+        if(MarkTracker==17){
+            TheMarker.transform.position = mark18.transform.position;
+        }
+        if(MarkTracker==18){
+            TheMarker.transform.position = mark19.transform.position;
+        }
+        if(MarkTracker==19){
+            TheMarker.transform.position = mark20.transform.position;
+        }
+        if(MarkTracker==20){
+            TheMarker.transform.position = mark21.transform.position;
+        }
+        if(MarkTracker==21){
+            TheMarker.transform.position = mark22.transform.position;
+        }
+        if(MarkTracker==22){
+            TheMarker.transform.position = mark23.transform.position;
+        }
+        if(MarkTracker==23){
+            TheMarker.transform.position = mark24.transform.position;
+        }
+        if(MarkTracker==24){
+            TheMarker.transform.position = mark25.transform.position;
+        }
+        if(MarkTracker==25){
+            TheMarker.transform.position = mark26.transform.position;
+        }
+        if(MarkTracker==26){
+            TheMarker.transform.position = mark27.transform.position;
+        }
+        if(MarkTracker==27){
+            TheMarker.transform.position = mark28.transform.position;
+        }
+        if(MarkTracker==28){
+            TheMarker.transform.position = mark29.transform.position;
+        }
+        if(MarkTracker==29){
+            TheMarker.transform.position = mark30.transform.position;
+        }
+        if(MarkTracker==30){
+            TheMarker.transform.position = mark31.transform.position;
+        }
 
     }
 
@@ -79,7 +154,7 @@ public class WaypointTrackerAI : MonoBehaviour
         if(other.gameObject.tag=="AICAR1"){
             this.GetComponent<BoxCollider>().enabled= false;
             MarkTracker +=1;
-            if(MarkTracker>=12){
+            if(MarkTracker>=30){
                 MarkTracker = 0;
             }
         }
