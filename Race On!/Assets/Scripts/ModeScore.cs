d830103e77e36589275b33de6513ab365eee542c
Rshetty02 <70402171+Rshetty02@users.noreply.github.com>
2020-10-25 15:35:47 +0530

    Added Score mode and Time Mode

---
+++ Race On!/Assets/Scripts/ModeScore.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModeScore : MonoBehaviour
+{
+    public int ModeSelection;
+    public GameObject RaceUI;
+    public GameObject ScoreUI;
+    public static int CurrentScore;
+    public int InternalScore;
+
+    public GameObject ScoreValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ModeSelection = ModeSelect.RaceMode;
+
+        if(ModeSelection == 1){
+            RaceUI.SetActive(false);
+            ScoreUI.SetActive(true);
+
+
+        }
+        
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InternalScore = CurrentScore;
+        
+            ScoreValue.GetComponent<Text>().text = "" + InternalScore;
+        
+    }
+}
---
+++ Race On!/Assets/Scripts/RedScore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedScore : MonoBehaviour
+{
+
+    void OnCollisionEnter(){
+        ModeScore.CurrentScore = ModeScore.CurrentScore + 100;
+        gameObject.SetActive(false);
+    }
+}
