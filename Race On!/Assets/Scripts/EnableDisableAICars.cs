2267938a4f346ab12f6d48ea667da61487e250b3
Rshetty02 <70402171+Rshetty02@users.noreply.github.com>
2020-10-27 23:40:59 +0530

    Added AI Car Selection

---
+++ Race On!/Assets/Scripts/EnableDisableAICars.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDisableAICars : MonoBehaviour
+{
+    public GameObject AICar1;
+    public GameObject AICar2;
+    public GameObject AICar3;
+    public GameObject AICar4;
+    public GameObject AICar5;
+    public GameObject AICar6;
+    public GameObject AICar7;
+    public GameObject AICar8;
+    public GameObject AICar9;
+    public GameObject AICar10;
+    public GameObject AICar11;
+    
+        // Start is called before the first frame update
+    void Start()
+    {
+        if(NoofAICars.NoOfAICarsvar ==3){
+            AICar1.SetActive(true);
+            AICar2.SetActive(true);
+            AICar3.SetActive(true);
+            AICar4.SetActive(false);
+            AICar5.SetActive(false);
+            AICar6.SetActive(false);
+            AICar7.SetActive(false);
+            AICar8.SetActive(false);
+            AICar9.SetActive(false);
+            AICar10.SetActive(false);
+            AICar11.SetActive(false);
+        }
+        else if(NoofAICars.NoOfAICarsvar ==7){
+            AICar1.SetActive(true);
+            AICar2.SetActive(true);
+            AICar3.SetActive(true);
+            AICar4.SetActive(true);
+            AICar5.SetActive(true);
+            AICar6.SetActive(true);
+            AICar7.SetActive(true);
+            AICar8.SetActive(false);
+            AICar9.SetActive(false);
+            AICar10.SetActive(false);
+            AICar11.SetActive(false);
+        }
+        else if(NoofAICars.NoOfAICarsvar ==9){
+            AICar1.SetActive(true);
+            AICar2.SetActive(true);
+            AICar3.SetActive(true);
+            AICar4.SetActive(true);
+            AICar5.SetActive(true);
+            AICar6.SetActive(true);
+            AICar7.SetActive(true);
+            AICar8.SetActive(true);
+            AICar9.SetActive(true);
+            AICar10.SetActive(false);
+            AICar11.SetActive(false);
+        }
+        else if(NoofAICars.NoOfAICarsvar ==11){
+            AICar1.SetActive(true);
+            AICar2.SetActive(true);
+            AICar3.SetActive(true);
+            AICar4.SetActive(true);
+            AICar5.SetActive(true);
+            AICar6.SetActive(true);
+            AICar7.SetActive(true);
+            AICar8.SetActive(true);
+            AICar9.SetActive(true);
+            AICar10.SetActive(true);
+            AICar11.SetActive(true);
+        }
+
+        else if(NoofAICars.NoOfAICarsvar ==0){
+            AICar1.SetActive(true);
+            AICar2.SetActive(false);
+            AICar3.SetActive(false);
+            AICar4.SetActive(false);
+            AICar5.SetActive(false);
+            AICar6.SetActive(false);
+            AICar7.SetActive(false);
+            AICar8.SetActive(false);
+            AICar9.SetActive(false);
+            AICar10.SetActive(false);
+            AICar11.SetActive(false);
+        }
+    
+    }
+
+   
+}
---
+++ Race On!/Assets/Scripts/NoofAICars.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoofAICars : MonoBehaviour
+{
+
+    public static int NoOfAICarsvar;
+
+    // Start is called before the first frame update
+    
+    public void ThreeAICars(){
+
+        NoOfAICarsvar = 3;
+
+    }
+
+    public void SevenAICars(){
+        NoOfAICarsvar = 7;
+    }
+
+    public void NineAICars(){
+        NoOfAICarsvar = 9;
+    }
+
+    public void ElevenAICars(){
+        NoOfAICarsvar = 11;
+    }
+
+
+}
