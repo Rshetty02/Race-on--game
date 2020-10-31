0463ad1a47042b3eca77806bf38c69d552a88cbe
Rshetty02 <70402171+Rshetty02@users.noreply.github.com>
2020-10-31 15:08:07 +0530

    Added ads

--- Race On!/Assets/Scripts/RedScore.cs
+++ Race On!/Assets/Scripts/RedScore.cs
@@ -13,7 +13,7 @@ public class RedScore : MonoBehaviour
         if(other.gameObject.tag=="Player"){
             
 
-            ModeScore.CurrentScore = ModeScore.CurrentScore + 100;
+            ModeScore.CurrentScore = ModeScore.CurrentScore + 10;
             gameObject.SetActive(false);
             
         }
